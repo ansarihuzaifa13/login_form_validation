1f35c73979396e8cd6f4abab16cc1a35b1cb051f
huzaifa <huzaifa.ansari@wealth-right.com>
2025-08-11 16:23:41 +0530

    first commit

---
+++ lib/bloc/auth_bloc.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AuthLoginRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        final email = event.email;
+      final password = event.password;
+
+      if (password.length < 6) {
+       return emit(AuthFailure('Password must be at least 6 characters long.'));
+      }
+
+      await Future.delayed(const Duration(seconds:1), () {
+        return emit(AuthSuccess(uid: '$email - $password'));
+      }
+      
+      );
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
+      
+    });
+    on<AuthLogoutRequested>((event, emit )async {
+      emit (AuthLoading());
+      try {
+await Future.delayed(const Duration(seconds:1), () {
+        return emit(AuthInitial());
+      });
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
+    });
+  }
+}
